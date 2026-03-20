8a4995c5eb1b166146be364041094c71519f7ff5
Becerritoo <111158373+Becerritoo@users.noreply.github.com>
2026-03-20 07:24:26 +0000

    fix: route slf4j-simple hikari logs to stdout at bootstrap

--- bukkit/src/main/java/com/alessiodp/parties/bukkit/bootstrap/BukkitPartiesBootstrap.java
+++ bukkit/src/main/java/com/alessiodp/parties/bukkit/bootstrap/BukkitPartiesBootstrap.java
@@ -4,7 +4,22 @@ import com.alessiodp.core.bukkit.bootstrap.ADPBukkitBootstrap;
 import com.alessiodp.parties.bukkit.BukkitPartiesPlugin;
 
 public class BukkitPartiesBootstrap extends ADPBukkitBootstrap {
+	static {
+		configureSlf4jSimpleLoggerDefaults();
+	}
+
 	public BukkitPartiesBootstrap() {
 		plugin = new BukkitPartiesPlugin(this);
+	}
+
+	private static void configureSlf4jSimpleLoggerDefaults() {
+		setIfAbsent("org.slf4j.simpleLogger.logFile", "System.out");
+		setIfAbsent("org.slf4j.simpleLogger.log.com.alessiodp.parties.libs.hikari", "warn");
+	}
+
+	private static void setIfAbsent(String key, String value) {
+		if (System.getProperty(key) == null) {
+			System.setProperty(key, value);
+		}
 	}
 }
